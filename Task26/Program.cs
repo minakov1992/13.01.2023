4f17d8aaa434aa408080c48472fd0452b1a3a69b
Slava <minakovwn@gmail.com>
2023-01-24 11:28:08 +0300

    task28

--- Task26/Program.cs
+++ Task26/Program.cs
@@ -7,6 +7,8 @@
 int CountNumbers(int num)
 {
     int count = 0;
+    if(num < 0) num = num * -1;
+
     while (num > 0)
     {
         num = num / 10;
---
+++ Task28/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N
+// и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int Factorial(int num)
+{
+    int prod = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        prod *= i;
+    }
+    return prod;
+}
+
+Console.Write("Введите число: ");
+int number =  Convert.ToInt32(Console.ReadLine());
+
+if(number > 0)
+{
+    int result = Factorial(number);
+    Console.WriteLine($"Произведение чисел от 1 до {number} равна {result}");
+}
+else
+    Console.WriteLine("Введено некорректное значение");
