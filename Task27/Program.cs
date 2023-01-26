14ff2105e3fbfede36b7e3df6ba1a1d8bcb5eb6a
Slava <minakovwn@gmail.com>
2023-01-27 00:58:26 +0300

    homework4update

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -3,14 +3,13 @@
 // 82 -> 10
 // 9012 -> 12
 
-// Рабочее решение.
 int SumDigits(int num)
 {
     int sum = 0;
-    while (num != 0)
+    if (num < 0) num *= -1;
+    for (int i = 0; i != num; i++)
     {
-        int value = num % 10;
-        sum += value;
+        sum += num % 10;
         num /= 10;
     }
     return sum;
@@ -28,16 +27,3 @@ else
     int result = SumDigits(number) < 0 ? SumDigits(-number) : SumDigits(number);
     Console.WriteLine($"Сумма цифр в числе {number}: {result}");
 }
-
-// Не работает на примере: 23456 -> 18, а должно быть 20.
-// int SumDigits(int num)
-// {
-//     int sum = 0;
-//     if (num <= 0) num *= -1;
-//     for (int i = 0; i <= num; i++)
-//     {
-//         sum += num % 10;
-//         num /= 10;
-//     }
-//     return sum;
-// }
